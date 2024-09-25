ae68e7b6a612c1c531134610daee3d6b05e58c54
Akshathaa21 <165242510+Akshathaa21@users.noreply.github.com>
2024-09-25 13:23:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+        var number = prompt("Enter a number:");
+        if (number % 2 == 0)
+        document.write(EVEN NUMBER)
+    else
+        document.write(ODD NUMBER)
+    </script>
